f61ba580d2a86c8450b3ec610973914ea5becc6a
romanglom <romanglom@gmail.com>
2013-04-20 12:51:12 -0300

    Comiteo proyecto base.

---
+++ Tp Luigi/hola.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int j = 0;
+	int i;
+
+	for(i=0;i<100;i++) {
+		j = j + 1;
+	}
+	
+	printf("El numero es:%d\n",j);
+	printf("Nico es un puto");
+	
+	return 0;
+}
