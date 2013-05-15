82dd6d62c0823cd5c837b8929918a01638646cc9
julian c <chapo.pr@hotmail.com>
2013-05-15 15:26:34 -0300

    loggie todo lo referido a orquestador y planificador hasta ahora.

--- Plataforma/src/Orquestador.c
+++ Plataforma/src/Orquestador.c
@@ -37,12 +37,13 @@ int iniciarOrquestador() {
 	niveles = dictionary_create();
 	planificadores = dictionary_create();
 
-	logger = log_create("/home/utnso/plataforma.log", "TEST", true,
+	logger = log_create("/home/utnso/orquestador.log", "TEST", true,
 			LOG_LEVEL_TRACE);
-	log_info(logger, "Log creado con exito, se procede levantar el servidor");
+	log_info(logger, "Log creado con exito");
 
+	log_debug(logger, "se va a iniciar el servidor");
 	socketEscucha = iniciarServidor(PUERTO);
-
+	log_debug(logger, "servidor escuchando en socket:%d y puerto:%d\n",socketEscucha,PUERTO);
 	printf("Escuchando conexiones entrantes.\n");
 
 // Aceptar una nueva conexion entrante. Se genera un nuevo socket con la nueva conexion.
@@ -56,20 +57,21 @@ int iniciarOrquestador() {
 			return EXIT_FAILURE;
 		}
 		log_info(logger, "Se acepto una conexion correctamente.");
-		log_info(logger, "se crea un hilo para manejar la conexion.");
+		log_debug(logger, "se crea un hilo para manejar la conexion.");
 		pthread_create(thread, NULL, (void*) manejarConexion,
 				socketNuevaConexion);
-
+		log_debug(logger, "hilo iniciado.");
 	}
 
 	close(socketEscucha);
 	log_info(logger, "Se serro el socket de escuchar de la plataforma.");
+	log_destroy(logger);
 	return EXIT_SUCCESS;
 }
 
 void manejarConexion(int* socket) {
 	MPS_MSG mensaje;
-
+	log_debug(logger, "Bloqueado esperando un msj");
 	recibirMensaje(*socket, &mensaje);
 	log_info(logger, "Se recibio un mensaje tipo: %d",
 			mensaje.PayloadDescriptor);
@@ -77,9 +79,11 @@ void manejarConexion(int* socket) {
 
 	case REGISTRAR_NIVEL:
 		log_info(logger, "Se conecto un nivel");
+		log_debug(logger, "Se va a deserializar el msj.");
 		NivelDatos *nivelDatos = NivelDatos_desserializer(mensaje.Payload);
+		log_debug(logger, "Se va a registrar el nivel.");
 		registrarNivel(nivelDatos, *socket);
-
+		log_debug(logger, "se va a iniciar un planificador");
 		iniciarUnPlanificador(nivelDatos->nombre);
 
 		log_info(logger, "Se creo un hilo para planificar para ese nivel");
@@ -90,25 +94,28 @@ void manejarConexion(int* socket) {
 		log_info(logger, "Se conecto un personaje.");
 		NivelConexion *nivel = malloc(sizeof(NivelConexion));
 		char* nombreNivel = (char*) mensaje.Payload;
-
+		log_debug(logger, "Se van a preparar los datos del Nivel: %c",nombreNivel);
 		int resultado=prepararNivelConexion(nombreNivel, nivel);
 
 		if (resultado == -1){
+			log_error(logger, "no esta el nivel");
 			enviarMsjError(socket,"no esta el nivel");
 			break;
 		}
 		if (resultado == -2) {
+			log_error(logger, "no esta el planificador.");
 			enviarMsjError(socket,"no esta el planificador");
 			break;
 		}
+		log_debug(logger, "Se van a serializar los datos del Nivel: %c.",nombreNivel);
 		t_stream *stream = nivelConexion_serializer(nivel);
-
+		log_debug(logger, "Asignamos los datos serializados en el msj.");
 		mensaje.PayloadDescriptor=PJ_PIDE_NIVEL;
 		mensaje.PayLoadLength=stream->length;
 		mensaje.Payload=stream->data;
-
+		log_debug(logger, "Se va a enviar el msj.");
 		enviarMensaje(*socket, &mensaje);
-		log_info(logger, "se lo deriva al orquestador");
+		log_debug(logger, "mensajeEnviado.");
 		break;
 	default:
 		log_error(logger, "Tipo de mensaje desconocido.");
@@ -119,14 +126,19 @@ void manejarConexion(int* socket) {
 
 void enviarMsjError(int *socket, char* msjError) {
 	MPS_MSG respuestaError;
+	log_debug(logger, "Se prepara msj de error.");
 	respuestaError.PayloadDescriptor = ERROR_MENSAJE;
 	respuestaError.PayLoadLength = strlen("msjError") + 1;
 	respuestaError.Payload = "msjError";
+	log_debug(logger, "Se va a enviar el msj de error.");
 	enviarMensaje(*socket, &respuestaError);
+	log_debug(logger, "Msj enviado correctamente.");
 }
 
 int registrarNivel(NivelDatos *nivelDatos, int socket) {
+
 	Nivel* nivel = malloc(sizeof(Nivel));
+	log_debug(logger, "Se va encolar el nivel.");
 	nivel->ip = nivelDatos->ip;
 	nivel->nombre = nivelDatos->nombre;
 	nivel->puerto = nivelDatos->puerto;
@@ -134,26 +146,31 @@ int registrarNivel(NivelDatos *nivelDatos, int socket) {
 	pthread_mutex_lock( &semaforo_niveles);
 	dictionary_put(niveles, nivel->nombre, nivel);
 	pthread_mutex_unlock( &semaforo_niveles);
+	log_debug(logger, "Nivel encolado.");
 	return 0;
 }
 
 int prepararNivelConexion(char* nombre, NivelConexion *nivelConexion) {
 	Nivel *nivel = malloc(sizeof(Nivel));
 	Planificador *planificador = malloc(sizeof(Planificador));
+	log_debug(logger, "Se busca el nivel.");
 	pthread_mutex_lock( &semaforo_niveles);
 	nivel=(Nivel*)dictionary_get(niveles,nombre);
 	pthread_mutex_unlock( &semaforo_niveles);
+	log_debug(logger, "Nivel ok.");
 
 	if(nivel==NULL)
 		return -1;
+	log_debug(logger, "Se busca el planificador.");
 	pthread_mutex_lock( &semaforo_planificadores);
 	planificador=(Planificador*)dictionary_get(planificadores,nombre);
 	pthread_mutex_lock( &semaforo_planificadores);
 	if (planificador==NULL)
 		return -2;
-
+	log_debug(logger, "Planificador ok.");
+	log_debug(logger, "Se va a armar la struct de NivelConexion con los datos.");
 	armarNivelConexion(nivelConexion,nivel,planificador);
-
+	log_debug(logger, "Struct ok.");
 	return 0;
 
 
@@ -167,8 +184,8 @@ void armarNivelConexion(NivelConexion *nivelConexion,Nivel *nivel,Planificador *
 }
 
 int iniciarUnPlanificador(char* nombreNivel) {
-pthread_t thread;
-pthread_create(&thread, NULL, (void*) iniciarPlanificador, (void*) nombreNivel);
-return 0;
+	pthread_t thread;
+	pthread_create(&thread, NULL, (void*) iniciarPlanificador, (void*) nombreNivel);
+	return 0;
 }
 
--- Plataforma/src/Planificador.c
+++ Plataforma/src/Planificador.c
@@ -42,29 +42,35 @@ int iniciarPlanificador(void* nombreNivel) {
 	//inicializarQuantum(); todo
 
 	socketEscucha = malloc(sizeof(int));
+	log_debug(logger, "Se inicia la conexion de escucha del planificador.");
 	planificador->puerto = realizarConexion(socketEscucha);
+	log_debug(logger, "Conexion de escucha en socket:%d , puerto:%d.",socketEscucha,planificador->puerto);
 	planificador->nombreNivel = (char*) nombreNivel;
 	planificador->ip = "127.0.0.1"; //todavia no se como conseguirla
 	planificador->personajes = list_create();
 	planificador->bloqueados = list_create();
+	log_debug(logger, "Se agrega en la lista compartida de planificadores.");
 	agregarmeEnPlanificadores(planificador);
 	pthread_t thread2;
+	log_debug(logger, "Se crea un hilo que va a dirigir los movimientos.");
 	pthread_create(&thread2, NULL, (void*) dirigirMovimientos, NULL );
 	while (1) {
 		thread = malloc(sizeof(pthread_t));
 		socketNuevaConexion = malloc(sizeof(int));
+		log_debug(logger, "Bloqueados esperando una conexion.");
 		if ((*socketNuevaConexion = accept(*socketEscucha, NULL, 0)) < 0) {
 			log_error(logger, "Error al aceptar una conexión.");
 			perror("Error al aceptar conexion entrante");
 			return EXIT_FAILURE;
 		}
-		log_info(logger, "Se acepto una conexion correctamente.");
-		log_info(logger, "se crea un hilo para manejar la conexion.");
+		log_info(logger, "Se acepto una conexion correctamente en el socket: %d.",*socketNuevaConexion);
+		log_info(logger, "se crea un hilo para recibir al personaje.");
 		pthread_create(thread, NULL, (void*) recibirPersonaje,
 				socketNuevaConexion);
 
 	}
-
+	log_debug(logger, "Se cierra el planificar del nivel:%s",planificador->nombreNivel);
+	log_destroy(logger);
 	return (EXIT_SUCCESS);
 }
 
@@ -88,43 +94,59 @@ int notificarMovimientoPermitido(int socketPersonaje) {
 void recibirPersonaje(int* socket) {
 	MPS_MSG mensajeInicializar;
 	Personaje* personaje = malloc(sizeof(Personaje));
-
+	log_debug(logger, "Esperando msj para inicializar un personaje.");
 	recibirMensaje(*socket, &mensajeInicializar);
-
+	log_debug(logger, "Mensaje recibido, seteamos personaje.");
 	personaje->simbolo = *(char*)mensajeInicializar.Payload;
 	personaje->socket = *socket;
 	personaje->quantum = quantumDefault;
+	log_debug(logger, "Encolamos el personaje.");
 	pthread_mutex_lock(&semaforo_listos);
 	cantPersonajes++;
 	queue_push(personajesListos, personaje);
 	pthread_mutex_unlock(&semaforo_listos);
+	log_debug(logger, "Personaje encolado.");
 }
 void dirigirMovimientos() {
 	Personaje *pj;
 	MPS_MSG respuesta;
 	while (1) {
-
+		log_debug(logger, "Mientras la cola de personajes este vacia, esperamos.");
 		while (queue_is_empty(personajesListos))
 			sleep(1);
+		log_debug(logger, "Tomamos el primer personaje de la cola.");
 		pj = queue_peek(personajesListos);
+		log_debug(logger, "Le notificamos el movimiento permitido.");
 		notificarMovimientoPermitido(pj->socket);
 		pj->quantum--;
+		log_debug(logger, "Esperamos la respuesta.");
 		recibirMensaje(pj->socket, &respuesta);
-
+		log_debug(logger, "Respuesta recibida:%c.",*(char*)respuesta.Payload);
 		switch (respuesta.PayloadDescriptor) {
 		case MOVIMIENTO_FINALIZADO:
+			log_debug(logger, "El quantum del pj es:%d.",pj->quantum);
 			if(pj->quantum==0){
+				log_debug(logger, "se saca el personaje de la cola.");
 				queue_pop(personajesListos);
+				log_debug(logger, "se le asigna denuevo el quantum.");
 				pj->quantum = quantumDefault;
+				log_debug(logger, "Lo volvemos a encolar.");
 				queue_push(personajesListos,pj);
+			}else{
+				log_debug(logger, "Notificamos que puede moverse nuevamente.");
+				notificarMovimientoPermitido(pj->socket);
 			}
 			break;
 		case BLOQUEADO:
+			log_debug(logger, "Sacamos personaje de la cola.");
 			queue_pop(personajesListos);
+			log_debug(logger, "lo agregamos en la lista de bloqueados.");
 			list_add(planificador->bloqueados,pj);
 			break;
 		case NIVEL_FINALIZADO:
+			log_debug(logger, "Sacamos personaje de la cola.");
 			queue_pop(personajesListos);
+			log_debug(logger, "Cerramos el socket por el que nos comunicabamos con el personaje.");
 			close(pj->socket);
 			break;
 		default:
